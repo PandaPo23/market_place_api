15d949868ca0082c1eef8b2fa9c4b08841f8e62f
PandaPo23 <pandapo1223@gmail.com>
2022-08-12 16:25:42 -0700

    Set the routes constraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #Api definition
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+    #We are going to list our resources here
+  end
 end
